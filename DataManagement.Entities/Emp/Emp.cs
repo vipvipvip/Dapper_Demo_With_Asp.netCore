00a1863811673bc24a9197f01c9ee307200313a2
vipvipvip <dikesh.chokshi@cogent6.com>
2020-12-27 13:28:15 -0500

    Initial commit

---
+++ DataManagement.Entities/Emp/Emp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DataManagement.Entities
+{
+    public class Emp
+    {
+        public int Id { get; set; }
+        public string EmpName { get; set; }
+        public string EmpAddress { get; set; }
+        public string EmailId { get; set; }
+        public string MobileNum { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ DataManagement.Repository/Repository/EmpRepository.cs
@@ -0,0 +1,84 @@
+﻿using Dapper;
+using DataManagement.Entities;
+using DataManagement.Repository.Interfaces;
+using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace EmpManagement.Repository
+{
+    public class EmpRepository<T> : IEmpRepository<Emp>
+    {
+        private readonly IConfiguration _config;
+        public EmpRepository(IConfiguration config)
+        {
+            _config = config;
+        }
+        internal IDbConnection DbConnection => new SqlConnection(_config.GetConnectionString("SQLDBConnectionString"));
+        public void AddEmp(Emp objEmp)
+        {
+            using (IDbConnection con = DbConnection)
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("EmpName", objEmp.EmpName);
+                parameters.Add("EmpAddress", objEmp.EmpAddress);
+                parameters.Add("EmailId", objEmp.EmailId);
+                parameters.Add("MobileNum", objEmp.MobileNum);
+                parameters.Add("Country", objEmp.Country);
+                parameters.Add("State", objEmp.State);
+                parameters.Add("City", objEmp.City);
+                parameters.Add("Image", objEmp.Image);
+                SqlMapper.Execute(con, "AddEmp", param: parameters, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public void DeleteEmp(int Id)
+        {
+            using (IDbConnection con = DbConnection)
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("Id", Id);
+                SqlMapper.Execute(con, "DeleteEmp", param: parameters, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public IEnumerable<Emp> GetAllEmp()
+        {
+            using (IDbConnection con = DbConnection)
+            {
+                return SqlMapper.Query<Emp>(con, "GetAllEmps", commandType: CommandType.StoredProcedure);
+            }
+
+        }
+
+        public Emp GetEmpById(int Id)
+        {
+            using (IDbConnection con = DbConnection)
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("Id", Id);
+                return SqlMapper.Query<Emp>(con, "GetEmpById", parameters, commandType: CommandType.StoredProcedure).FirstOrDefault();
+            }
+
+        }
+        public void UpdateEmp(Emp objEmp)
+        {
+            using (IDbConnection con = DbConnection)
+            {
+                DynamicParameters parameters = new DynamicParameters();
+                parameters.Add("EmpName", objEmp.EmpName);
+                parameters.Add("EmpAddress", objEmp.EmpAddress);
+                parameters.Add("EmailId", objEmp.EmailId);
+                parameters.Add("MobileNum", objEmp.MobileNum);
+                parameters.Add("Country", objEmp.Country);
+                parameters.Add("State", objEmp.State);
+                parameters.Add("City", objEmp.City);
+                parameters.Add("Image", objEmp.Image);
+                parameters.Add("Id", objEmp.Id);
+                SqlMapper.Execute(con, "UpdateEmp", param: parameters, commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
